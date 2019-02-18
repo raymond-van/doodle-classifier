10b7054626f493cd1a4fe8e05105e2c03dd100af
raymond-van <vraymond028@gmail.com>
2019-02-18 03:41:09 -0500

    Created keras model and converted it to tfjs

---
+++ cnn.py
@@ -0,0 +1,51 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from tensorflow.keras import layers
+from tensorflow import keras 
+import tensorflow as tf
+from data import X, y, class_names
+
+num_classes = len(class_names)
+
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=19)
+
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+# Define model
+model = keras.Sequential()
+model.add(layers.Convolution2D(16, (3, 3),
+                        padding='same',
+                        input_shape=x_train.shape[1:], activation='relu'))
+model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(layers.Convolution2D(32, (3, 3), padding='same', activation= 'relu'))
+model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(layers.Convolution2D(64, (3, 3), padding='same', activation= 'relu'))
+model.add(layers.MaxPooling2D(pool_size =(2,2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dense(50, activation='softmax')) 
+# Train model
+adam = tf.train.AdamOptimizer()
+model.compile(loss='categorical_crossentropy',
+              optimizer=adam,
+              metrics=['top_k_categorical_accuracy'])
+print(model.summary())
+
+model.fit(x = x_train, y = y_train, validation_split=0.1, batch_size = 256, verbose=2, epochs=5)
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test accuarcy: {:0.2f}%'.format(score[1] * 100))
+
+import matplotlib.pyplot as plt
+from random import randint
+idx = randint(0, len(x_test))
+img = x_test[idx]
+plt.imshow(img.squeeze()) 
+plt.show()
+pred = model.predict(np.expand_dims(img, axis=0))[0]
+ind = (-pred).argsort()[:5]
+latex = [class_names[x] for x in ind]
+print(latex)
+
+model.save('model/qd_classifier.h5')
